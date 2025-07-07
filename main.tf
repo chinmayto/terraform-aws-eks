18982b9a85c98482cf38ee6740b9c4944f2353a5
chinmayto <chinmay13@gmail.com>
2025-07-07 23:20:22 +0530

    Commit

---
+++ main.tf
@@ -0,0 +1,59 @@
+data "aws_availability_zones" "available_zones" {
+  state = "available"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr_block
+
+  azs = slice(data.aws_availability_zones.available_zones.names, 0, 3)
+
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name        = var.vpc_name
+    Environment = var.environment
+    Terraform   = "true"
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = var.eks_cluster_name
+  cluster_version = "1.32"
+
+  # Optional
+  cluster_endpoint_public_access = true
+
+  # Optional: Adds the current caller identity as an administrator via cluster access entry
+  enable_cluster_creator_admin_permissions = true
+
+  eks_managed_node_groups = {
+    example = {
+      instance_types = ["t3.medium"]
+      min_size       = 1
+      max_size       = 5
+      desired_size   = 2
+    }
+  }
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  tags = {
+    Name        = var.eks_cluster_name
+    Environment = var.environment
+    Terraform   = "true"
+  }
+
+}
---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS region to use for resources."
+  default     = "us-east-1"
+}
+
+variable "vpc_name" {
+  type        = string
+  description = "Name of the VPC"
+  default     = "VPC-EKS-Cluster"
+}
+
+variable "vpc_cidr_block" {
+  type        = string
+  description = "Base CIDR Block for VPC"
+  default     = "10.0.0.0/16"
+}
+
+
+variable "eks_cluster_name" {
+  type        = string
+  description = "Name of the EKS Cluster"
+  default     = "CT-EKS-Cluster"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment for deployment"
+  default     = "DEV"
+}
+
+variable "instance_key" {
+  default = "WorkshopKeyPair"
+}
